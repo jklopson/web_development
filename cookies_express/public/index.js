0b99ffd4dccc8ead3c5ab439c731d05c67c20e4c
jklopson <jklopson23@gmail.com>
2023-05-04 14:15:21 -0400

    cookies lab

--- cookies_express/public/index.js
+++ cookies_express/public/index.js
@@ -1,8 +1,9 @@
+// Run: node index.js
 var express = require('express')
 var app = express();
 let https = require('https')
 
-
+// -------------- express initialization -------------- //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,20 +13,16 @@ app.set('view engine', 'ejs')
 // -------------- express 'get' handlers -------------- //
 app.get('/', (req,res) =>{
 
-	let cookie_name = 'JKLOPAPAPAPAPA'
-	let cookie_value = '0293847LALALALALALA'
+	let cookie_name = 'this_is_a_cookie'
+	let cookie_value = '0111011'
 
 	res.cookie(cookie_name, cookie_value, {maxAge: 360000})
-	
-    console.log('Cookies: ', req.cookies)
-
 	res.render('template', {cookie_name: cookie_name, cookie_value: cookie_value})
 })
-
 
 // -------------- listener -------------- //
 // // The listener is what keeps node 'alive.' 
 
-var listener = app.listen(process.env.PORT || 8080, process.env.HOST || "0.0.0.0", function() {
+var listener = app.listen(process.env.PORT || 8088, process.env.HOST || "0.0.0.0", function() {
     console.log("Express server started");
 });
